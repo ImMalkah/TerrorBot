1a20720ab5aa525c1c96029a0b25e35ffc4b76a3
fadiyas <immalkah@icloud.com>
2022-01-23 03:21:13 -0500

    edited pom.xml file

--- src/main/java/events/SpamTrap.java
+++ src/main/java/events/SpamTrap.java
@@ -13,18 +13,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class SpamTrap extends ListenerAdapter {
     private Map<User, Integer> warned = new HashMap<>();
 
     public void onMessageReceived(MessageReceivedEvent event) {
         List<Message> history = event.getTextChannel().getIterableHistory().complete().stream().limit(7) // gets last 10 messages in channel
-                .filter(msg -> !msg.equals(event.getMessage())).toList(); // filters out the last message sent (does not include it)
+                .filter(msg -> !msg.equals(event.getMessage())).collect(Collectors.toList()); // filters out the last message sent (does not include it)
         int spam = history.stream()
                 // gets the author of the previous message if it's the same author as the message sent and makes sure it's not a bot
                 .filter(message -> message.getAuthor().equals(event.getAuthor()) && !message.getAuthor().isBot())
                 // message time sent - last seen message time sent if it's less than 6 seconds
-                .filter(msg -> (event.getMessage().getTimeCreated().toEpochSecond() - msg.getTimeCreated().toEpochSecond() < 6)).toList().size();
+                .filter(msg -> (event.getMessage().getTimeCreated().toEpochSecond() - msg.getTimeCreated().toEpochSecond() < 6)).collect(Collectors.toList()).size();
         if (spam > 2 && !Objects.equals(event.getGuild().getOwner(), event.getMember()) && !Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR)) {
             int timesWarned = 0;
             if (warned.containsKey(event.getAuthor()))
